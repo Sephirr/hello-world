863982c49fa7f7caa017989832936cc084c3d2db
Sephirr <54677386+Sephirr@users.noreply.github.com>
2019-08-29 17:07:30 +0200

    Create ooze.py

---
+++ ooze.py
@@ -0,0 +1,13 @@
+import random
+  przymiotnik =
+  ["Vile", "Scavenging", "Saturated", "Sap", "Rotting", "Ravenous",
+   "Primordial", "Prima", "Plague", "Noxious", "Nightmare", "Necrotic",
+   "Mwonvuli", "Muculent", "Monstrous", "Mephitic", "Living", "Irradiated",
+   "Inexorable", "Green", "Gobbling", "Gluttonous", "Glutinous", "Fungal",
+   "Evolving", "Earthen", "Creeping", "Corrosive", "Consumptive",
+   "Cloned"] rzeczownik =
+  ["Sucker", "Spitter", "Sludge", "Slime", "Shambler", "Ooze", "Lurker",
+   "Goo", "Elastoplast", "Ectoplasm", "Decay", "Crawler", "Blob"] var1 =
+  random.choice (przymiotnik) var2 = random.choice (rzeczownik) var3 =
+  "Your ooze name is"
+  print (var3, var1, var2)
