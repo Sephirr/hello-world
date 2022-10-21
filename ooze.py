787058554b272d95af010ba826b5760c30272a9b
Sephirr <54677386+Sephirr@users.noreply.github.com>
2022-10-21 14:11:15 +0200

    Update ooze.py

--- ooze.py
+++ ooze.py
@@ -1,13 +1,8 @@
 import random
-  przymiotnik =
-  ["Vile", "Scavenging", "Saturated", "Sap", "Rotting", "Ravenous",
-   "Primordial", "Prima", "Plague", "Noxious", "Nightmare", "Necrotic",
-   "Mwonvuli", "Muculent", "Monstrous", "Mephitic", "Living", "Irradiated",
-   "Inexorable", "Green", "Gobbling", "Gluttonous", "Glutinous", "Fungal",
-   "Evolving", "Earthen", "Creeping", "Corrosive", "Consumptive",
-   "Cloned"] rzeczownik =
-  ["Sucker", "Spitter", "Sludge", "Slime", "Shambler", "Ooze", "Lurker",
-   "Goo", "Elastoplast", "Ectoplasm", "Decay", "Crawler", "Blob"] var1 =
+rzeczownik = ["Rolnictwo","Edukacja i rehabilitacja","Przygotowanie pedagogiczne","Integracja sensoryczna","Zarządzanie","Język angielski","Nauczanie plastyki","Oligofrenopedagogika","Edukacja włączająca","Dietetyka","Coaching","Doradztwo zawodowe","Zielarstwo","Eliksiry","Bibliotekoznastwo","Nauczanie matematyki","Bezpieczeństwo i higiena pracy","Psychotraumatologia"]
+  przymiotnik =["dla absolwentów kierunków nierolniczych","z elementami coachingu","ze wstępem do edukacji Montessori",", autoprezentacja i wystąpienia publiczne","z arteterapią","w społeczeństwie","osób ze spektrum autyzmu","dla psychologów","i podstawy pszczelarstwa","według zasad Szanghajskich","dla urodzonych zwycięzców","z instrukcją zakupu i przekształcenia ziemi","oraz ochrona przeciwpożarowa (ppoż)"]
+  
+var1 =
   random.choice (przymiotnik) var2 = random.choice (rzeczownik) var3 =
   "Your ooze name is"
   print (var3, var1, var2)
