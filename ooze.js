8a7390dce61d8733abc13911c50a5acbde5b0012
Sephirr <54677386+Sephirr@users.noreply.github.com>
2022-10-21 14:14:56 +0200

    Update ooze.js

--- ooze.js
+++ ooze.js
@@ -1,12 +1,7 @@
-var rzeczownik = ["Sucker", "Spitter", "Sludge", "Slime", "Shambler", "Ooze", "Lurker","Goo","Elastoplast","Ectoplasm","Decay","Crawler","Blob"];
-  var przymiotnik =
-  ["Vile", "Scavenging", "Saturated", "Sap", "Rotting", "Ravenous",
-   "Primordial", "Prima", "Plague", "Noxious", "Nightmare", "Necrotic",
-   "Mwonvuli", "Muculent", "Monstrous", "Mephitic", "Living", "Irradiated",
-   "Inexorable", "Green", "Gobbling", "Gluttonous", "Glutinous", "Fungal",
-   "Evolving", "Earthen", "Creeping", "Corrosive", "Consumptive",
-   "Cloned"];
+var rzeczownik = ["Rolnictwo","Edukacja i rehabilitacja","Przygotowanie pedagogiczne","Integracja sensoryczna","Zarządzanie","Język angielski","Nauczanie plastyki","Oligofrenopedagogika","Edukacja włączająca","Dietetyka","Coaching","Doradztwo zawodowe","Zielarstwo","Eliksiry","Bibliotekoznastwo","Nauczanie matematyki","Bezpieczeństwo i higiena pracy","Psychotraumatologia"];
+  var przymiotnik =["dla absolwentów kierunków nierolniczych","z elementami coachingu","ze wstępem do edukacji Montessori",", autoprezentacja i wystąpienia publiczne","z arteterapią","w społeczeństwie","osób ze spektrum autyzmu","dla psychologów","i podstawy pszczelarstwa","według zasad Szanghajskich","dla urodzonych zwycięzców","z instrukcją zakupu i przekształcenia ziemi","oraz ochrona przeciwpożarowa (ppoż)"]
+  ;
    var rzecz1 = rzeczownik[Math.floor(Math.random()*rzeczownik.length)];
    var przym1 = przymiotnik[Math.floor(Math.random()*przymiotnik.length)];
-   var outp ="Your slime name is "+ przym1+" "+rzecz1;
+   var outp ="Twój kierunek to "+ przym1+" "+rzecz1;
    window.alert(outp);
